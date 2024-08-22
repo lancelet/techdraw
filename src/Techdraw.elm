0c2801955b641c8fbda07b27992af9dec26936ff
Jonathan Merritt <j.s.merritt@gmail.com>
2024-08-22 12:13:11 +1000

    Initial application of styles
    - Styles are converted to SVG.
    - Path decoration processing added.

--- src/Techdraw.elm
+++ src/Techdraw.elm
@@ -2,12 +2,15 @@ module Techdraw exposing
     ( Drawing
     , render
     , empty, path, svg, group, transform
-    , withStyle
-    , withFill, withStroke, withStrokeWidth
+    , style
+    , fill, stroke, strokeWidth
     , Style
     , styleDefault, styleInheritAll
-    , fill, stroke, strokeWidth
-    , appendAttribute
+    , styleSetFill, styleSetStroke, styleSetStrokeWidth
+    , styleGetFill, styleGetStroke, styleGetStrokeWidth
+    , styleAppendDecorator, styleAppendAttribute
+    , styleGetDecorators, styleGetAttributes
+    , Decorator(..)
     )
 
 {-|
@@ -44,16 +47,23 @@ TODO:
 
 ## Styling drawings
 
-@docs withStyle
-@docs withFill, withStroke, withStrokeWidth
+@docs style
+@docs fill, stroke, strokeWidth
 
 
 # Styles
 
 @docs Style
 @docs styleDefault, styleInheritAll
-@docs fill, stroke, strokeWidth
-@docs appendAttribute
+@docs styleSetFill, styleSetStroke, styleSetStrokeWidth
+@docs styleGetFill, styleGetStroke, styleGetStrokeWidth
+@docs styleAppendDecorator, styleAppendAttribute
+@docs styleGetDecorators, styleGetAttributes
+
+
+# Path Decoration
+
+@docs Decorator
 
 -}
 
@@ -67,9 +77,9 @@ import Techdraw.Math as Math
         , toSvgPPath
         )
 import TypedSvg exposing (g)
-import TypedSvg.Attributes exposing (d)
+import TypedSvg.Attributes as SvgAttributes
 import TypedSvg.Core exposing (Svg)
-import TypedSvg.Types exposing (Paint)
+import TypedSvg.Types exposing (Paint, px)
 import VirtualDom exposing (Attribute)
 
 
@@ -82,7 +92,7 @@ import VirtualDom exposing (Attribute)
 type Drawing msg
     = DrawingEmpty
     | DrawingPath Path
-    | DrawingSvg (Svg msg)
+    | DrawingSvg (Style msg -> AffineTransform -> Svg msg)
     | DrawingStyled (Style msg) (Drawing msg)
     | DrawingTransformed AffineTransform (Drawing msg)
     | DrawingGroup (List (Drawing msg))
@@ -103,8 +113,12 @@ path =
 
 
 {-| Draw some SVG directly.
+
+The function provided takes the current style and local-to-world
+transformation so that it can produce custom SVG.
+
 -}
-svg : Svg msg -> Drawing msg
+svg : (Style msg -> AffineTransform -> Svg msg) -> Drawing msg
 svg =
     DrawingSvg
 
@@ -134,8 +148,8 @@ The style is applied "on the outside". Any styles already defined will take
 precedence.
 
 -}
-withStyle : Style msg -> Drawing msg -> Drawing msg
-withStyle parentStyle drawing =
+style : Style msg -> Drawing msg -> Drawing msg
+style parentStyle drawing =
     case drawing of
         DrawingStyled childStyle dwg ->
             -- Any existing child style applied to the drawing takes
@@ -148,32 +162,32 @@ withStyle parentStyle drawing =
 
 {-| Set the fill.
 
-See [`withStyle`](#withStyle) for information on precedence.
+See [`style`](#style) for information on precedence.
 
 -}
-withFill : Paint -> Drawing msg -> Drawing msg
-withFill paint drawing =
-    withStyle (styleDefault |> fill paint) drawing
+fill : Paint -> Drawing msg -> Drawing msg
+fill paint drawing =
+    style (styleDefault |> styleSetFill paint) drawing
 
 
 {-| Set the stroke.
 
-See [`withStyle`](#withStyle) for information on precedence.
+See [`style`](#style) for information on precedence.
 
 -}
-withStroke : Paint -> Drawing msg -> Drawing msg
-withStroke paint drawing =
-    withStyle (styleDefault |> stroke paint) drawing
+stroke : Paint -> Drawing msg -> Drawing msg
+stroke paint drawing =
+    style (styleDefault |> styleSetStroke paint) drawing
 
 
 {-| Set the stroke width.
 
-See [`withStyle`](#withStyle) for information on precedence.
+See [`style`](#style) for information on precedence.
 
 -}
-withStrokeWidth : Float -> Drawing msg -> Drawing msg
-withStrokeWidth width drawing =
-    withStyle (styleDefault |> strokeWidth width) drawing
+strokeWidth : Float -> Drawing msg -> Drawing msg
+strokeWidth width drawing =
+    style (styleDefault |> styleSetStrokeWidth width) drawing
 
 
 
@@ -185,6 +199,18 @@ withStrokeWidth width drawing =
 type StyleSetting a
     = Inherited
     | Set a
+
+
+{-| Convert a `StyleSetting` to a `Maybe` value.
+-}
+styleSettingToMaybe : StyleSetting a -> Maybe a
+styleSettingToMaybe setting =
+    case setting of
+        Inherited ->
+            Nothing
+
+        Set value ->
+            Just value
 
 
 {-| Combine a style setting from a parent and child into a new current style
@@ -210,6 +236,7 @@ type Style msg
         { fill : StyleSetting Paint
         , stroke : StyleSetting Paint
         , strokeWidth : StyleSetting Float
+        , decorators : List (Decorator msg)
         , extraAttributes : List (Attribute msg)
         }
 
@@ -233,29 +260,68 @@ styleInheritAll =
         { fill = Inherited
         , stroke = Inherited
         , strokeWidth = Inherited
+        , decorators = []
         , extraAttributes = []
         }
 
 
 {-| Set the fill.
 -}
-fill : Paint -> Style msg -> Style msg
-fill paint (Style style) =
-    Style <| { style | fill = Set paint }
+styleSetFill : Paint -> Style msg -> Style msg
+styleSetFill paint (Style styl) =
+    Style <| { styl | fill = Set paint }
+
+
+{-| Return the fill setting.
+-}
+styleGetFill : Style msg -> Maybe Paint
+styleGetFill (Style styl) =
+    styl.fill |> styleSettingToMaybe
 
 
 {-| Set the stroke.
 -}
-stroke : Paint -> Style msg -> Style msg
-stroke paint (Style style) =
-    Style <| { style | stroke = Set paint }
+styleSetStroke : Paint -> Style msg -> Style msg
+styleSetStroke paint (Style styl) =
+    Style <| { styl | stroke = Set paint }
+
+
+{-| Return the stroke setting.
+-}
+styleGetStroke : Style msg -> Maybe Paint
+styleGetStroke (Style styl) =
+    styl.stroke |> styleSettingToMaybe
 
 
 {-| Set the stroke width.
 -}
-strokeWidth : Float -> Style msg -> Style msg
-strokeWidth width (Style style) =
-    Style <| { style | strokeWidth = Set width }
+styleSetStrokeWidth : Float -> Style msg -> Style msg
+styleSetStrokeWidth width (Style styl) =
+    Style <| { styl | strokeWidth = Set width }
+
+
+{-| Return the stroke width.
+-}
+styleGetStrokeWidth : Style msg -> Maybe Float
+styleGetStrokeWidth (Style styl) =
+    styl.strokeWidth |> styleSettingToMaybe
+
+
+{-| Append a decorator.
+-}
+styleAppendDecorator : Decorator msg -> Style msg -> Style msg
+styleAppendDecorator decorator (Style styl) =
+    Style <| { styl | decorators = decorator :: styl.decorators }
+
+
+{-| Return the decorators.
+
+The decorators are returned in the sequence they should be applied.
+
+-}
+styleGetDecorators : Style msg -> List (Decorator msg)
+styleGetDecorators (Style styl) =
+    styl.decorators |> List.reverse
 
 
 {-| Append a custom SVG attribute.
@@ -265,9 +331,19 @@ supported by the library. However, any provided attribute will be appended
 to the output SVG.
 
 -}
-appendAttribute : Attribute msg -> Style msg -> Style msg
-appendAttribute attribute (Style style) =
-    Style <| { style | extraAttributes = attribute :: style.extraAttributes }
+styleAppendAttribute : Attribute msg -> Style msg -> Style msg
+styleAppendAttribute attribute (Style styl) =
+    Style <| { styl | extraAttributes = attribute :: styl.extraAttributes }
+
+
+{-| Return the attributes.
+
+The attributes are returned in the sequence they should be applied.
+
+-}
+styleGetAttributes : Style msg -> List (Attribute msg)
+styleGetAttributes (Style styl) =
+    styl.extraAttributes |> List.reverse
 
 
 {-| Combine settings from a parent style and child style into a new
@@ -283,8 +359,38 @@ combineStyles (Style parent) (Style child) =
         { fill = comb .fill
         , stroke = comb .stroke
         , strokeWidth = comb .strokeWidth
+        , decorators = child.decorators ++ parent.decorators
         , extraAttributes = child.extraAttributes ++ parent.extraAttributes
         }
+
+
+
+---- Path Decoration ----------------------------------------------------------
+
+
+{-| A decorator is a special styling operation that can operate on a
+processed path.
+
+A decorator runs on a path after:
+
+  - A collapsed style has been computed (taking into account the styles
+    of groups above the path, etc)
+  - The local-to-world transformation has been computed.
+  - The path has been transformed to world space.
+
+A `Decorator` can then modify the path as it sees fit, to produce a new
+[`Drawing`](#Drawing). The original `Drawing` is discarded, so if the
+aim is to retain it, then it must be part of the `Drawing` returned by
+the `Decorator`.
+
+-}
+type Decorator msg
+    = Decorator
+        (Style msg
+         -> AffineTransform
+         -> Path
+         -> Drawing msg
+        )
 
 
 
@@ -341,29 +447,80 @@ use something like a CEK machine.
 -}
 renderWithState : State msg -> Drawing msg -> Svg msg
 renderWithState state drawing =
+    let
+        styl : Style msg
+        styl =
+            state |> stateStyle
+
+        localToWorld : AffineTransform
+        localToWorld =
+            state |> stateLocalToWorld
+
+        decorators : List (Decorator msg)
+        decorators =
+            styl |> styleGetDecorators
+    in
     case drawing of
         DrawingEmpty ->
             g [] []
 
         DrawingPath pth ->
-            TypedSvg.path
-                [ -- Transform the path and convert it to SVG
-                  affApplyPath (stateLocalToWorld state) pth
-                    |> toSvgPPath
-                    |> d
+            if List.isEmpty decorators then
+                -- The style has no decorators. This means the path has no
+                -- further processing required and will be dumped to SVG
+                -- directly.
+                let
+                    styleAttributes =
+                        styleToAttributes styl
 
-                -- TODO: Add styles here.
-                ]
-                []
+                    pathAttr =
+                        affApplyPath localToWorld pth |> toSvgPPath |> SvgAttributes.d
 
-        DrawingSvg svgChild ->
-            svgChild
+                    attributes =
+                        pathAttr :: styleAttributes
+                in
+                TypedSvg.path attributes []
 
-        DrawingStyled style childDrawing ->
-            renderWithState (stateCombineStyles state style) childDrawing
+            else
+                -- The style has decorators, so we must process them to
+                -- produce a new drawing. At this point, we strip out all
+                -- the previous transformations and style, since the
+                -- decorator has access to those directly.
+                let
+                    xfpath =
+                        affApplyPath localToWorld pth
+                in
+                List.map (\(Decorator f) -> f styl localToWorld xfpath) decorators
+                    |> group
+                    |> renderWithState initState
+
+        DrawingSvg mkSvgChild ->
+            mkSvgChild styl localToWorld
+
+        DrawingStyled parentStyle childDrawing ->
+            renderWithState (stateCombineStyles state parentStyle) childDrawing
 
         DrawingTransformed childToParent childDrawing ->
             renderWithState (stateComposeTransform state childToParent) childDrawing
 
         DrawingGroup children ->
             g [] <| List.map (renderWithState state) children
+
+
+{-| Convert a style to a list of SVG attributes.
+-}
+styleToAttributes : Style msg -> List (Attribute msg)
+styleToAttributes styl =
+    let
+        toAttr : (Style msg -> Maybe a) -> (a -> Attribute msg) -> Maybe (Attribute msg)
+        toAttr extract produce =
+            extract styl |> Maybe.map produce
+
+        styleAttrs =
+            List.filterMap (\x -> x)
+                [ toAttr styleGetFill SvgAttributes.fill
+                , toAttr styleGetStroke SvgAttributes.stroke
+                , toAttr styleGetStrokeWidth (\x -> SvgAttributes.strokeWidth (px x))
+                ]
+    in
+    styleAttrs ++ List.reverse (styleGetAttributes styl)
